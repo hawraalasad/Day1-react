bf93b0b088bb477c44b63f8f62636fe49a9b59ea
hawraalasad <hawraalasad@yahoo.com>
2024-08-18 20:11:48 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import shop from "./assets/media/millie-s-cookie-shop-york-yorkshire-uk-BARE9Y.jpg";
+import "./assets/css/container.css";
+import items from "./assets/Data/Data";
 
 function App() {
+  const itemsDisplay = items.map((item) => (
+    <div className="item-container">
+      <img src={item.image} alt="cookies" className="item-image" />
+      <h3>{item.name}</h3>
+      <p>{item.price}</p>
+    </div>
+  ));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-div">
+      <div className="name">
+        <h1>Cookies and Beyond</h1>
+        <h5>Where cookies maniacs gather</h5>
+        <img src={shop} alt="shop" className="image" />
+      </div>
+      <div className="divs">{itemsDisplay}</div>
     </div>
   );
 }
---
+++ src/assets/Data/Data.js
@@ -0,0 +1,16 @@
+const items = [
+  {
+    name: "Chocolate chips cookies",
+    image:
+      "https://images.aws.nestle.recipes/resized/5b069c3ed2feea79377014f6766fcd49_Original_NTH_Chocolate_Chip_Cookie_1080_850.jpg",
+    price: "2KD",
+  },
+  {
+    name: "Cute cookies",
+    image:
+      "https://i.pinimg.com/originals/0b/1d/77/0b1d772ec10c445288f553dbc3cc7280.jpg",
+    price: "2KD",
+  },
+];
+
+export default items;
